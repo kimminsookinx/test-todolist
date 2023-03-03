ae1c34bf010279ef907bc71cbc015cc31f214e12
kimminsoo <kimminsoo@kinx.net>
2023-03-03 09:12:11 +0900

    base url change v2 -> v3

--- router/routes.go
+++ router/routes.go
@@ -8,7 +8,7 @@ import "github.com/kimminsookinx/test-todolist/controllers"
 */
 
 func createRoute() {
-	todoRoute := r.Group("/v2/todos")
+	todoRoute := r.Group("/v3/todos")
 	{
 		todoController := new(controllers.TodoController)
 
